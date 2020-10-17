1e43a13e63c684d8a5f2e20355daf36930effbb8
Alexander Weinberger <alexanderweinberger@gmx.at>
2020-10-17 11:26:38 +0200

    Added description

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-const product = 'Socks'
+// Create Vue instance
+const app = Vue.createApp({
+    data(){
+        return{
+            product: 'Socks',
+            description: 'A nice pair of footweare!'
+        }
+    }
+})
+
